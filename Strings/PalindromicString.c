e2aaee9ce5180ea2a06da793346e32c674836903
Muskan <53108044+muskangoel21@users.noreply.github.com>
2020-01-13 14:50:33 +0530

    Add files via upload

---
+++ Strings/PalindromicString.c
@@ -0,0 +1,29 @@
+//https://www.interviewbit.com/problems/palindrome-string/
+bool isAlphaNumeric(char c){
+    if((c>='0'&&c<='9')||(c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+     return true;
+    return false;
+}
+int Solution::isPalindrome(string A) {
+    int i=0,len=0;
+    while(A[len]!='\0'){
+     len++;
+    }
+    int j=len-1;
+    while(i<=j){
+       // cout<<A[i]<<" "<<A[j]<<endl;
+        if(!isAlphaNumeric(A[i])){
+         i++;
+         continue;
+        }
+        if(!isAlphaNumeric(A[j])){
+         j--;
+         continue;
+        }
+        if(A[i]!=A[j]&&abs(A[i]-A[j])!=32)
+         return 0;
+        
+        i++;j--;
+    }
+    return 1;
+}
