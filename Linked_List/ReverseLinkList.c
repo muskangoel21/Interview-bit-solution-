e1f90ad583b52809eb804a67d0d7c40e46b5c038
Muskan <53108044+muskangoel21@users.noreply.github.com>
2020-01-26 11:34:38 +0530

    Update ReverseLinkList.c

--- Linked_List/ReverseLinkList.c
+++ Linked_List/ReverseLinkList.c
@@ -1,37 +1,61 @@
 //https://www.interviewbit.com/problems/reverse-link-list-ii/
 listnode* reverseBetween(listnode* A, int B, int C) 
 {
-   listnode *ptr=A;
-   listnode *p=A;
-   listnode *curr=ptr;
-   listnode *next;
-   listnode *prev=NULL;
-   int count=1;
-   if(B==C)
-   {
-       return A;
-   }
-   while(ptr)
-   {
-       if(count<B)
-       {
-           p=ptr;
-          ptr=ptr->next; 
-          printf("%d ",ptr->val);
-       }
-       else if(count>=B && count<=C)
-       {
-           next=curr->next;
-           curr->next=prev;
-           prev=curr;
-           curr=next;
-       }
-       else
-       {
-           p=prev;
-           ptr=ptr->next;
-       }
-       count++;
-   }
-   return ;
+    listnode*curr=A;
+    listnode*prev=NULL;
+    listnode*next=NULL;
+    listnode *temp=A;
+    listnode *temp1=A;
+    int count =1,flag=0;
+    if(B==C)
+    {
+        return A;
+    }
+    while(curr!=NULL)
+    {
+      if(count <B)
+      {
+          temp=curr;
+          curr=curr->next;
+          temp1=curr;
+      }
+      else if(count<=C)
+    {
+        
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+    }
+    else
+    {
+        if(B==1)
+         {
+           A=prev;
+           break;
+            }
+            else
+            {
+                temp->next=prev;
+                temp1->next=curr;
+                flag=1;
+                break;
+            }
+         }
+      
+      count++;
+      
+    }
+    if(B==1)
+    {
+     A=prev;   
+    }
+    if(flag==0)
+    {
+        temp->next=prev;
+        temp1->next=curr;
+    }
+    
+     
+    return A;
 }
